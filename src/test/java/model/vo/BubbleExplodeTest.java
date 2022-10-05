92dbee4480d4d1cb2e083dab2a7159af9f35e99d
shenlihong <2936241486@qq.com>
2022-10-05 21:47:11 +0800

    部分测试

---
+++ src/test/java/model/vo/BubbleExplodeTest.java
@@ -0,0 +1,82 @@
+package model.vo;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BubbleExplodeTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void createExplode() {
+    }
+
+    @Test
+    void showElement() {
+    }
+
+    @Test
+    void move() {
+    }
+
+    @Test
+    void destroy() {
+    }
+
+    @Test
+    void crash() {
+    }
+
+    @Test
+    void setMoveStep() {
+    }
+
+    @Test
+    void getUp() {
+    }
+
+    @Test
+    void testSetUp() {
+    }
+
+    @Test
+    void getDown() {
+    }
+
+    @Test
+    void setDown() {
+    }
+
+    @Test
+    void getLeft() {
+    }
+
+    @Test
+    void setLeft() {
+    }
+
+    @Test
+    void getRight() {
+    }
+
+    @Test
+    void setRight() {
+    }
+
+    @Test
+    void getPlayerNum() {
+    }
+
+    @Test
+    void setPlayerNum() {
+    }
+}
---
+++ src/test/java/model/vo/NpcTest.java
@@ -0,0 +1,78 @@
+package model.vo;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NpcTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void createNpc() {
+    }
+
+    @Test
+    void showElement() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void judgeForward() {
+    }
+
+    @Test
+    void move() {
+    }
+
+    @Test
+    void destroy() {
+    }
+
+    @Test
+    void addBubble() {
+    }
+
+    @Test
+    void getMoveX() {
+    }
+
+    @Test
+    void setMoveX() {
+    }
+
+    @Test
+    void getNpcNum() {
+    }
+
+    @Test
+    void setNpcNum() {
+    }
+
+    @Test
+    void getImgW() {
+    }
+
+    @Test
+    void setImgW() {
+    }
+
+    @Test
+    void getImgH() {
+    }
+
+    @Test
+    void setImgH() {
+    }
+}
