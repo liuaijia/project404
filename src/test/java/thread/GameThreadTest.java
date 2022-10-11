6e7ebda36922d4d229c3bff7505517236e9310e0
shenlihong <2936241486@qq.com>
2022-10-11 18:51:49 +0800

    123

---
+++ src/test/java/thread/GameThreadTest.java
@@ -0,0 +1,38 @@
+package thread;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameThreadTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void playerBoom() {
+    }
+
+    @Test
+    void npcBoom() {
+    }
+
+    @Test
+    void fragilityBoom() {
+    }
+
+    @Test
+    void playerMagicBox() {
+    }
+
+    @Test
+    void npcMagicBox() {
+    }
+}
